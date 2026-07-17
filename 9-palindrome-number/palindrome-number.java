418db11d59f6ec5cbe776401fa2a0efb275c9c12
Arnav Mishra <arnav.m.mishra.07@gmail.com>
2026-07-18 03:51:28 +0530

    Time: 7 ms (12.14%) | Memory: 46.2 MB (17.17%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if (x < 0) {
+            return false;
+        }
+
+        int dup = x;
+        int revNum = 0;
+
+        while (x != 0) {
+            int lastdigit = x % 10;
+            revNum = revNum * 10 + lastdigit;
+            x /= 10;
+
+        }
+
+        return revNum == dup;
+    
+    }
+}
